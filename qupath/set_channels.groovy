c5e1da87689c839efe3ad5dd15b8195e9f612697
Christian Rickert <rc.login@icloud.com>
2025-10-06 13:41:29 -0600

    fix timing issue and add option to show specific channels

--- qupath/set_channels.groovy
+++ qupath/set_channels.groovy
@@ -15,7 +15,7 @@ this program. If not, see <https://www.gnu.org/licenses/>.
 
 Author:     Christian Rickert <christian.rickert@cuanschutz.edu>
 Date:       2025-10-06
-Version:    0.1
+Version:    0.2
 
 Hint:
 Load and execute this script with QuPath's script editor window using
@@ -28,18 +28,52 @@ for all projects opened in your current QuPath installation.
 BigDecimal minDisplay = 0.0
 BigDecimal maxDisplay = 4095.0
 
+// optional: show specific channels (fuzzy search)
+showChannels = ['all']
+
 // optional: set new names
 def newChannels = []
 
 // get image data
 def imageData = getCurrentImageData()
+def viewer = getCurrentViewer()
+def display = viewer.getImageDisplay()
 def server = imageData.getServer()
 def channels = server.getMetadata().getChannels()
+def selectedChannels = channels.findAll {
+    channel -> showChannels.any {
+        substring -> channel.getName().toLowerCase().contains(substring.toLowerCase())
+    }
+}
+
+// hide all channels (avoid timing issues)
+availableDisplayChannels = display.availableChannels()
+display.selectedChannels.setAll(display.availableChannels())
+display.selectedChannels.clear()
 
 // change channel display range
+activeChannels = []
 for (int c = 0; c < channels.size(); c++) {
+    if (selectedChannels.contains(channels[c])) {
+        activeChannels << channels[c]
+    }
     setChannelDisplayRange(c, minDisplay, maxDisplay)
 }
+
+// show all active channels
+if (showChannels.any { it.toLowerCase() == 'all' }) {
+    display.selectedChannels.setAll(availableDisplayChannels)
+    } else {
+    def displayChannels = availableDisplayChannels.findAll {
+        displayChannel ->
+            def displayName = displayChannel.toString()
+            selectedChannels*.getName().any {
+                selectedName -> selectedName.contains(displayName.split(" \\(")[0])
+            }
+    }
+    display.selectedChannels.setAll(displayChannels)
+}
+viewer.repaintEntireImage()
 
 // change channel names
 if (newChannels.size() == channels.size()) {
